4652c59ca0d9f7b28895915f4023a935974edbb0
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-11-20 10:41:29 +0530

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         </li>
         {/* </ul> */}
 
-        <ul className="ulTContainer">   
+        <ul className="ulTContainer">
           <li className="liStyl">
             <a href="#projects" className="liTText">
               Projects
@@ -40,21 +40,24 @@ function App() {
 
       <div className="wholeContainer">
         <div className="introCon">
-          <p className="name"><span stye={{color: 'white'}}>Hello,</span> This is Neelam Raikwar</p>
+          <p className="name">
+            <span stye={{ color: "white" }}>Hello,</span> This is Neelam Raikwar
+          </p>
           <h1 className="role">
             The <span>Full Stack Developer</span>
           </h1>
-          <hr  style={{ marginBottom: '0rem', padding: '0rem'}}/>
+          <hr style={{ marginBottom: "0rem", padding: "0rem" }} />
           <p className="about">
             {" "}
-            An enthusiastic full-stack developer creating effective digital
-            websites with modern technologies. Specializing in React,
-            Node.js, and MongoDB to build scalable and user friendly
-            applications.
+            A full-stack developer skilled in building modern web apps with
+            react, Node.js, and MongoDB. Focused on solving technical challenges
+            and delivering reliable end-to-end solutions.
           </p>
 
           <div className="btnContainer">
-            <a href="#connect"><button className="button">Contact Me</button></a>
+            <a href="#connect">
+              <button className="button">Contact Me</button>
+            </a>
             <button className="button">Resume</button>
           </div>
         </div>
@@ -64,7 +67,7 @@ function App() {
           <p className="about">
             Building solutions that solve real-world problems
           </p>
-          <hr/>
+          <hr />
 
           <div className="projectCont">
             <div className="projectBox">
@@ -76,7 +79,10 @@ function App() {
               <div className="projInfo">
                 <h2>Book Readers</h2>
                 <p className="aboutProj">
-                  A modern e-commerce web for buying books with the feature add to cart, wishlist and detail page, to filter books by categories, price, ratings and sorting option with price. Created with React.js, Bootstrap, MongoDB, Express, Node.js
+                  A modern e-commerce web for buying books with the feature add
+                  to cart, wishlist and detail page, to filter books by
+                  categories, price, ratings and sorting option with price.
+                  Created with React.js, Bootstrap, MongoDB, Express, Node.js
                 </p>
                 <div className="tecBtns">
                   <p>React</p>
@@ -89,13 +95,21 @@ function App() {
 
                 <div className="demoCodeBtns">
                   <button className="btn">
-                  <a href="https://my-book-lovers-shop.vercel.app/" target="_blank">
-                    <i class="bi bi-eye"></i>Demo
-                  </a>
+                    <a
+                      href="https://my-book-lovers-shop.vercel.app/"
+                      target="_blank"
+                    >
+                      <i class="bi bi-eye"></i>Demo
+                    </a>
                   </button>
-                  <button className="btn" style={{width: 'rem'}}>
-                  <a href="https://github.com/neelamraikwar9/myBookLoversShop" target="_blank">
-                    <i class="bi bi-code-slash"></i></a>Code
+                  <button className="btn" style={{ width: "rem" }}>
+                    <a
+                      href="https://github.com/neelamraikwar9/myBookLoversShop"
+                      target="_blank"
+                    >
+                      <i class="bi bi-code-slash"></i>
+                    </a>
+                    Code
                   </button>
                 </div>
               </div>
@@ -110,7 +124,10 @@ function App() {
               <div className="projInfo">
                 <h2>Anvaya CRM Dashboard</h2>
                 <p className="aboutProj">
-                   A CRM app to manage Leads with Sales Agent. It provide a best user performance. It has a feature to track leads by statuses and it provides add new lead and sales agents with the edit leads option and the comments.  
+                  A CRM app to manage Leads with Sales Agent. It provide a best
+                  user performance. It has a feature to track leads by statuses
+                  and it provides add new lead and sales agents with the edit
+                  leads option and the comments.
                 </p>
                 <div className="tecBtns">
                   <p>React</p>
@@ -124,14 +141,20 @@ function App() {
 
                 <div className="demoCodeBtns">
                   <button className="btn">
-                  <a href="https://anvaya-crm-app-azure.vercel.app/" target="_blank">
-                    <i class="bi bi-eye"></i>Demo
-                  </a>
+                    <a
+                      href="https://anvaya-crm-app-azure.vercel.app/"
+                      target="_blank"
+                    >
+                      <i class="bi bi-eye"></i>Demo
+                    </a>
                   </button>
                   <button className="btn">
-                  <a href="https://github.com/neelamraikwar9/AnvayaCRMApp" target="_blank">
-                    <i class="bi bi-code-slash"></i>Code
-                  </a>
+                    <a
+                      href="https://github.com/neelamraikwar9/AnvayaCRMApp"
+                      target="_blank"
+                    >
+                      <i class="bi bi-code-slash"></i>Code
+                    </a>
                   </button>
                 </div>
               </div>
@@ -139,236 +162,219 @@ function App() {
           </div>
         </section>
 
-
-        
         <section id="techStack">
-        <h1 className="role">Tech Stack</h1>
-        <p className="about">Technologies and tools I work with</p>
-        <hr/>
+          <h1 className="role">Tech Stack</h1>
+          <p className="about">Technologies and tools I work with</p>
+          <hr />
 
-        <div className="tackStackCon">
-          <div className="iconCon">
-            <img src="/icons/react.png" alt="React" className="icon" />
-            <p className="iconName">React</p>
-          </div>
+          <div className="tackStackCon">
+            <div className="iconCon">
+              <img src="/icons/react.png" alt="React" className="icon" />
+              <p className="iconName">React</p>
+            </div>
 
-          <div className="iconCon">
-            <img src="/icons/js.svg" alt="JavaScript" className="icon" />
-            <p className="iconName">JavaScript</p>
-          </div>
+            <div className="iconCon">
+              <img src="/icons/js.svg" alt="JavaScript" className="icon" />
+              <p className="iconName">JavaScript</p>
+            </div>
 
-          <div className="iconCon">
-            <img
-              src="/icons/html5.png"
-              alt="html5.png"
-              className="icon"
-            />
-            <p className="iconName">HTML5</p>
-          </div>
+            <div className="iconCon">
+              <img src="/icons/html5.png" alt="html5.png" className="icon" />
+              <p className="iconName">HTML5</p>
+            </div>
 
-          <div className="iconCon">
-            <img src="/icons/css3.png" alt="CSS3" className="icon" />
-            <p className="iconName">CSS3</p>
-          </div>
+            <div className="iconCon">
+              <img src="/icons/css3.png" alt="CSS3" className="icon" />
+              <p className="iconName">CSS3</p>
+            </div>
 
-          <div className="iconCon">
-            <img
-              src="/icons/nodejs.png"
-              alt="Node.js"
-              className="icon"
-            />
-            <p className="iconName">Node.js</p>
-          </div>
+            <div className="iconCon">
+              <img src="/icons/nodejs.png" alt="Node.js" className="icon" />
+              <p className="iconName">Node.js</p>
+            </div>
 
-          <div className="iconCon">
-            <img
-              src="/icons/express.png"
-              alt="Express"
-              className="icon expressIcon"
-            />
-            <p className="iconName">Express</p>
-          </div>
+            <div className="iconCon">
+              <img
+                src="/icons/express.png"
+                alt="Express"
+                className="icon expressIcon"
+              />
+              <p className="iconName">Express</p>
+            </div>
 
-          <div className="iconCon">
-            <img
-              src="/icons/mongodb.png"
-              alt="MongoDB"
-              className="icon"
-              style={{ width: "2rem" }}
-            />
-            <p className="iconName">MongoDB</p>
-          </div>
+            <div className="iconCon">
+              <img
+                src="/icons/mongodb.png"
+                alt="MongoDB"
+                className="icon"
+                style={{ width: "2rem" }}
+              />
+              <p className="iconName">MongoDB</p>
+            </div>
 
-          <div className="iconCon">
-            <img src="/icons/git.png" alt="GIT" className="icon" />
-            <p className="iconName">GIT</p>
-          </div>
+            {/* <div className="iconCon">
+              <img src="/icons/git.png" alt="GIT" className="icon" />
+              <p className="iconName">GIT</p>
+            </div> */}
 
-          <div className="iconCon">
-            <img
-              src="/icons/image.png"
-              alt="GitHub"
-              className="icon gitIcon"
-            />
-            <p className="iconName">GitHub</p>
-          </div>
+            <div className="iconCon">
+              <img
+                src="/icons/image.png"
+                alt="GitHub"
+                className="icon gitIcon"
+              />
+              <p className="iconName">GitHub</p>
+            </div>
 
-          <div className="iconCon">
-            <img
-              src="/icons/bootstrap5.png"
-              alt="Bootstrap"
-              className="icon"
-            />
-            <p className="iconName">Bootstrap</p>
-          </div>
+            <div className="iconCon">
+              <img
+                src="/icons/bootstrap5.png"
+                alt="Bootstrap"
+                className="icon"
+              />
+              <p className="iconName">Bootstrap</p>
+            </div>
 
-          <div className="iconCon vsIcon">
-            <img
-              src="/icons/vscode.svg"
-              alt="VS Code"
-              className="icon"
-            />
-            <p className="iconName">VS Code</p>
-          </div>
+            <div className="iconCon vsIcon">
+              <img src="/icons/vscode.svg" alt="VS Code" className="icon" />
+              <p className="iconName">VS Code</p>
+            </div>
 
-          <div className="iconCon">
-            <img
-              src="/icons/mongoose.png"
-              alt="Mongoose"
-              className="icon"
-            />
-            <p className="iconName">Mongoose</p>
-          </div>
+            <div className="iconCon">
+              <img src="/icons/mongoose.png" alt="Mongoose" className="icon" />
+              <p className="iconName">Mongoose</p>
+            </div>
 
-          <div className="iconCon">
-            <img
-              src="/icons/vercel.png"
-              alt="Vercel"
-              className="icon vercelIcon"
-              style={{ height: "3rem" }}
-            />
-            <p className="iconName">Vercel</p>
-          </div>
+            <div className="iconCon">
+              <img
+                src="/icons/vercel.png"
+                alt="Vercel"
+                className="icon vercelIcon"
+                style={{ height: "3rem" }}
+              />
+              <p className="iconName">Vercel</p>
+            </div>
 
-          <div className="iconCon">
-            <img
-              src="/icons/postman.png"
-              alt="Postman"
-              className="icon"
-            />
-            <h1 className="iconName">Postman</h1>
+            <div className="iconCon">
+              <img src="/icons/postman.png" alt="Postman" className="icon" />
+              <h1 className="iconName">Postman</h1>
+            </div>
           </div>
-        </div>
         </section>
-
 
         <section id="connect">
-        <h1 className="role">Connect</h1>
-        <p className="about">Let's collaborate on something amazing together</p>
-        <hr />
-        
-        <div className="connectCon">
+          <h1 className="role">Connect</h1>
+          <p className="about">
+            Let's collaborate on something amazing together
+          </p>
+          <hr />
 
-        <div className="connectContainer">
-          <div className="connectBoxes">
-          <img src="/socialIcons/email2.png" alt="Email Box" style={{width: '4rem', height: '4rem', margin: '2rem'}}/>
-            <h2 className="connHeadText">Email</h2>
-          </div>
-          <div>
-              <h3 className="connectext"><a href="neelam.raikwar.234303@gmail.com">neelam.raikwar.234303@gmail.com</a></h3>
+          <div className="connectCon">
+            <div className="connectContainer">
+              <div className="connectBoxes">
+                <img
+                  src="/socialIcons/email2.png"
+                  alt="Email Box"
+                  style={{ width: "4rem", height: "4rem", margin: "2rem" }}
+                />
+                <h2 className="connHeadText">Email</h2>
+              </div>
+              <div>
+                <h3 className="connectext">
+                  <a href="neelam.raikwar.234303@gmail.com">
+                    neelam.raik9@gmail.com
+                  </a>
+                </h3>
+              </div>
+            </div>
+
+            <div className="connectContainer">
+              <div className="connectBoxes">
+                <a
+                  href="https://github.com/neelamraikwar9"
+                  target="_blank"
+                  style={{ marginTop: "2rem" }}
+                >
+                  <i
+                    class="devicon-github-original"
+                    style={{
+                      color: "#f173c7ff",
+                      fontSize: "4rem",
+                      // border: "1px solid red",
+                      margin: "2rem",
+                      marginTop: "50rem",
+                    }}
+                  ></i>
+                </a>
+                <h2 className="connHeadText">GitHub</h2>
+              </div>
+              <div>
+                <h3 className="connectext">Follow on GitHub</h3>
+              </div>
+            </div>
+
+            <div className="connectContainer">
+              <div className="connectBoxes">
+                <a
+                  href="https://www.linkedin.com/in/neelam-raikwar-204614250/"
+                  target="_blank"
+                  style={{ marginTop: "2rem" }}
+                >
+                  <i
+                    class="devicon-linkedin-plain colored"
+                    style={{
+                      color: "#f173c7ff",
+                      fontSize: "4rem",
+                      // border: "1px solid red",
+                      margin: "2rem",
+                      height: "4rem",
+                    }}
+                  ></i>
+                </a>
+                <h2 className="connHeadText">LinkedIn</h2>
+              </div>
+              <div>
+                <h4 className="connectext">Connect on LinkedIn</h4>
+              </div>
+            </div>
+
+            <div className="connectContainer">
+              <div className="connectBoxes">
+                <a
+                  href="https://x.com/Neelam92004"
+                  alt="Twitter"
+                  style={{ marginTop: "2rem" }}
+                >
+                  <i
+                    class="devicon-twitter-original"
+                    style={{
+                      color: "#f173c7ff",
+                      fontSize: "4rem",
+                      // border: "1px solid red",
+                      margin: "2rem",
+                      height: "4rem",
+                    }}
+                  ></i>
+                </a>
+
+                <h2 className="connHeadText">Twitter</h2>
+              </div>
+              <h3 className="connectext">Follow on Twitter</h3>
             </div>
           </div>
-
-          <div className="connectContainer">
-            <div className="connectBoxes">
-            <a href="https://github.com/neelamraikwar9" target="_blank" style={{marginTop: '2rem'}}>
-              <i
-                class="devicon-github-original"
-                style={{
-                  color: "#f173c7ff",
-                  fontSize: "4rem",
-                  // border: "1px solid red",
-                  margin: "2rem",
-                  marginTop: '50rem'
-                  
-                }}
-              ></i>
-              </a>
-              <h2 className="connHeadText">GitHub</h2>
-            </div>
-            <div>
-              <h3 className="connectext">Follow on GitHub</h3>
-            </div>
-          </div>
-
-
-
-
-          <div className="connectContainer">
-          <div className="connectBoxes">
-          <a href="https://www.linkedin.com/in/neelam-raikwar-204614250/" target="_blank"  style={{marginTop: '2rem'}}>
-            <i
-              class="devicon-linkedin-plain colored"
-              style={{
-                color: "#f173c7ff",
-                fontSize: "4rem",
-                // border: "1px solid red",
-                margin: "2rem",
-                height: "4rem",
-              }}
-            ></i>
-            </a>
-            <h2 className="connHeadText">LinkedIn</h2>
-            </div>
-            <div>
-              <h4 className="connectext">Connect on LinkedIn</h4>
-            </div>
-          </div>                          
-
-
-
-
-
-
-
-
-
-           <div className="connectContainer">
-          <div className="connectBoxes">
-          <a href="https://x.com/Neelam92004" alt="Twitter"  style={{marginTop: '2rem'}}>
-            <i
-              class="devicon-twitter-original"
-              style={{
-                color: "#f173c7ff",
-                fontSize: "4rem",
-                // border: "1px solid red",
-                margin: "2rem",
-                height: "4rem",
-              }}
-            ></i>
-            </a>
-            
-            <h2 className="connHeadText">Twitter</h2>
-          </div>
-          <h3 className="connectext">Follow on Twitter</h3>
-          </div>
-        </div>
         </section>
       </div>
-      
-      
 
       <footer className="footContainer">
-      <div className="footerCon">
-        <p>&copy; 2025 Neelam Raikwar • All right reserved</p>
-        <p><strong>Built with using CSS3 and React.js</strong></p>
-        <p>Learning full stack development at neog.camp🩷</p>
-      </div>
+        <div className="footerCon">
+          <p>&copy; 2025 Neelam Raikwar • All right reserved</p>
+          <p>
+            <strong>Built with using CSS3 and React.js</strong>
+          </p>
+          <p>Learning full stack development at neog.camp🩷</p>
+        </div>
       </footer>
-
     </main>
-
-    
   );
 }
 
